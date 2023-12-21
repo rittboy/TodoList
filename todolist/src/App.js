72af6a5b04e031a5d77a9e158a18560a20720bf8
John Ritter <john.ritter.8788@gmail.com>
2023-12-21 05:47:12 -0500

    Continued to work out bugs with App file

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,11 @@
 import {useState} from 'react';
 import './App.css';
 
-function App() {
+function App(){
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
   const [updating, setUpdating] = useState(false);
+
   function addTodo(){
     if(newTodo.trim()){
       setTodos([...todos, {
@@ -18,49 +19,18 @@ function App() {
   }
 
   function updateTodo(id, newText){
-    setTodos(todos.map(todo =>(todo.id === id ?{
+    setTodos(todos.map(todo =>(todo.id === id ? {
       ...todo,
       text: newText
-    } : todo)));
+    } : todo )));
   }
 
   function toggleTodo(id){
-    setTodos(todos.map(todo =>(todo.id === id ?{
+    setTodos(todos.map(todo =>(todo.id === id ? {
       ...todo,
       completed: !todo.completed
     } : todo)));
   }
 
-  function deleteTodo(id){
-    setTodos(todos.filter(todo => todo.id !== id));
-  }
-
-  function toggleUpdating(id){
-    setUpdating(!updating);
-    setTodos(todos.map(todo =>(todo.id === id ?{
-      ...todo,
-      updating: !todo.updating
-    } : todo)));
-  }
-  return(
-    <div className='App'>
-      <h1>React To - Do App</h1>
-      <input type='text'
-      placeholder='Add a new to-do'
-      value={
-        newTodo
-      }
-      onChange={
-        e => setNewTodo(e.target.value)
-      }
-      onKeyDown={
-        e =>(e.key === 'Enter' ? addTodo() : null)
-      }
-      /><ul>
-        {
-          
-        }
-      </ul>
-    </div>
-  )
+  
 }
